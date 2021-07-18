cb62792d7986cbb3f29976991a923b0da45e2a11
Ping <panfilo.glophics@gmail.com>
2021-07-18 11:56:44 +0800

    repo

--- execute/map.php
+++ execute/map.php
@@ -1,29 +1,21 @@
-<style>
-    .cottages:hover {
-        fill: blue;
-        cursor: pointer;
-    }
-</style>
 <?php
     /* database connection */
     require_once('../library/config.php');
     //TENTS
     $tents = $dbConn->query("SELECT * FROM tbl_amenities WHERE status = 'av' AND category_id = 2")->fetch(PDO::FETCH_ASSOC);
-    
     //TABLES
     $tables = $dbConn->query("SELECT * FROM tbl_amenities WHERE status = 'av' AND category_id = 6")->fetch(PDO::FETCH_ASSOC);
 
     //CHAIRS
     $chairs = $dbConn->query("SELECT * FROM tbl_amenities WHERE status = 'av' AND category_id = 1")->fetch(PDO::FETCH_ASSOC);
-    
     //COTTAGES
     $cottages = $dbConn->query("SELECT * FROM tbl_amenities WHERE status = 'av' AND category_id = 3");
-
     //ROOMS
     $rooms = $dbConn->query("SELECT * FROM tbl_amenities WHERE status = 'av' AND category_id = 5");
-
- 
 ?>
+<style>
+    .cottages:hover {fill: #FFFFFF;cursor: pointer;}
+</style>
 <div id="map_design">
     <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
         width="960px" height="960px" viewBox="0 0 960 960" enable-background="new 0 0 960 960" xml:space="preserve">
@@ -44,12 +36,13 @@
             <?php echo $tables['quantity']; ?>
         </text>
         
-        <!-- CHAIRS -->
+        <!-- =================== START CHAIRS ============================= -->
         <rect class="cottages" x="639.5" y="481.5" fill="#0dd00d" stroke="#000000" stroke-miterlimit="10" width="157" height="77"/>
         <text transform="matrix(1 0 0 1 688.8379 506.1602)" font-family="'ArialMT'" font-size="20">CHAIRS</text>
         <text transform="matrix(1 0 0 1 692.2793 546.2559)" font-family="'ArialMT'" font-size="41.5568">
             <?php echo $chairs['quantity']; ?>
         </text>
+        <!-- =================== END CHAIRS =============================== -->
         
         <line fill="none" stroke="#000000" stroke-miterlimit="10" x1="526.5" y1="897" x2="526.5" y2="960"/>
         <line fill="none" stroke="#000000" stroke-miterlimit="10" x1="467.5" y1="897" x2="467.5" y2="960"/>
@@ -62,7 +55,7 @@
         <text transform="matrix(1 0 0 1 334 677.5)"><tspan x="0" y="0" font-family="'ArialMT'" font-size="12">SWIMMING </tspan><tspan x="0" y="14.4" font-family="'ArialMT'" font-size="12">    POOL</tspan></text>
         <text transform="matrix(1 0 0 1 222.7632 680.5)" font-family="'ArialMT'" font-size="12">RESTO BAR</text>
 
-        <!-- ROOMS -->
+        <!-- =================== START ROOMS =================================== -->
         <?php 
         while ($room = $rooms->fetch(PDO::FETCH_OBJ)) { ?>
             <rect 
@@ -79,14 +72,8 @@
                 font-family="'ArialMT'" 
                 font-size="12"><?php echo $room->name ?></text>
         <?php } ?>
-        <!-- <rect class="cottages" x="98.5" y="693.5" fill="#FFFFFF" stroke="#000000" stroke-miterlimit="10" width="77" height="47"/>
-        <text transform="matrix(1 0 0 1 113.2637 722)" font-family="'ArialMT'" font-size="12">ROOM 4</text>
-        <rect class="cottages" x="98.5" y="751.5" fill="#FFFFFF" stroke="#000000" stroke-miterlimit="10" width="77" height="47"/>
-        <text transform="matrix(1 0 0 1 114.2637 781)" font-family="'ArialMT'" font-size="12">ROOM 3</text>
-        <rect class="cottages" class="cottages" x="98.5" y="810.5" fill="#FFFFFF" stroke="#000000" stroke-miterlimit="10" width="77" height="47"/>
-        <text transform="matrix(1 0 0 1 114.2637 837)" font-family="'ArialMT'" font-size="12">ROOM 2</text>
-        <rect class="cottages" x="98.5" y="865.5" fill="#FFFFFF" stroke="#000000" stroke-miterlimit="10" width="77" height="47"/>
-        <text transform="matrix(1 0 0 1 113.2637 894.4736)" font-family="'ArialMT'" font-size="12">ROOM 1</text> -->
+        <!-- =================== END ROOMS ======================================== -->
+        <text transform="matrix(1 0 0 1 25.5913 372.8813)" font-family="'ArialMT'" font-size="20">COTTAGES</text>
         <!-- =================== START COTTAGES =================================== -->
         <?php 
         while ($cottage = $cottages->fetch(PDO::FETCH_OBJ)) { ?>
@@ -106,18 +93,19 @@
         <?php } ?>
         <!-- =================== END COTTAGES =================================== -->
         
-        <text transform="matrix(1 0 0 1 25.5913 372.8813)" font-family="'ArialMT'" font-size="20">COTTAGES</text>
-        
+        <!-- =================== START TENTS ==================================== -->
         <rect class="cottages" x="25.5" y="101.5" fill="#0dd00d" stroke="#000000" stroke-miterlimit="10" width="232" height="77"/>
         <text transform="matrix(1 0 0 1 104.7847 124.1606)" font-family="'ArialMT'" font-size="20">TENTS</text>
         <text transform="matrix(1 0 0 1 112.0005 163.2559)" font-family="'ArialMT'" font-size="41.5568">
             <?php echo isset($tents['quantity']) > 0 ? $tents['quantity'] : 0; ?>
         </text>
-        
+        <!-- =================== END TENTS ======================================= -->
+        <!-- =================== START FUNCTION HALL ============================= -->
         <rect class="cottages" x="208.5" y="753.5" fill="#0dd00d" stroke="#000000" stroke-miterlimit="10" width="204" height="175"/>
         <text transform="matrix(1 0 0 1 267.8271 834)">
             <tspan x="0" y="0" font-family="'ArialMT'" font-size="20">FUNCTION</tspan>
             <tspan x="0" y="24" font-family="'ArialMT'" font-size="20">HALL</tspan>
         </text>
+        <!-- =================== end FUNCTION HALL =============================== -->
     </svg>
 </div>
